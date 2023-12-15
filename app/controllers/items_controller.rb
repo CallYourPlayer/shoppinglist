0fa2f0effdc5e1488ba3cfd5e2fe8912471250ab
e5098 <guest.torre.a@igt.com>
2023-12-15 18:53:11 +0100

    provo ajax calculate price, incrociamo tutto

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -81,16 +81,9 @@ class ItemsController < ApplicationController
 		@item.update_attribute :total_price, @item.quantity * @item.unit_price
 		@shopping =Shopping.find(@item.shopping_id)
 		@shopping.total_price = @shopping.total
-		#if @item.update(item_params)
-	    	@shopping.total_price = @shopping.total
-	    	@shopping.save
-	      	redirect_to edit_shopping_path(@shopping)
-	    #else
-	      #render :edit, status: :unprocessable_entity
-	    #end
-	#	respond_to do |format|
-	#	  format.js {render 'calculateprice'}
-	#	end
+		respond_to do |format|
+			format.js 
+		end
 	end
 
 	private
