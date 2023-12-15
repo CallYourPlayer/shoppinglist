692214ffdac24a56a46fd6e72ec4513cf8be1264
e5098 <guest.torre.a@igt.com>
2023-12-15 22:01:45 +0100

    rimpiccoliti bottoni in tabella home, pagata redirect to show

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -107,7 +107,7 @@ class ShoppingsController < ApplicationController
     @shopping = Shopping.find(params[:id])
 
     if @shopping.update(shopping_params)
-      redirect_to @shoppings
+      redirect_to @shopping
     else
       render :edit, status: :unprocessable_entity
     end
